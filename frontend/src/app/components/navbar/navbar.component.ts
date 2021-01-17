57ebb6a4442f1d7b8dea6113c9735345b877c921
jankos1195 <jankos1195@gmail.com>
2021-01-17 13:34:04 +0100

    ➕ add navigating on user pages

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  userPages: string[] = ["/profile", "/flats", "/assignments", "/shopping", "/billings", "/ranking"];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  getActualRoute() {
+    return(this.router.url); 
+  }
+
+  checkIfActual(buttonName: string): string {
+    let endPoint = this.getActualRoute();
+    let buttonEndPintValue: string = "/" + buttonName;
+    if (buttonEndPintValue == endPoint) {
+      return "color:#69f0af"
+    } else {
+      return ""
+    }
+  }
+
+  checkIfMenu(): boolean {
+    let endPoint = this.getActualRoute();
+    if (this.userPages.includes(endPoint)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  changePage(page: string): void {
+    let navigatePage = "/" + page;
+    this.router.navigate([navigatePage]);
+  }
+
+  logOutUser(): void {
+    // TODO: ADD Auth service
+    // this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
