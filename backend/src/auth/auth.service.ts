a8fcebf4e48d2b59a8afcf2c547bde8b65f1a26f
Jacek Starz <jacek.starz@gmail.com>
2021-01-22 20:40:10 +0100

    ♻️ Change jwt token to hold only user id

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { email: user.email, sub: user.id };
+    const payload = { sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
