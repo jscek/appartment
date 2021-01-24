31f40a7a23e9b0ff40e664377f98b535c1aeb225
Jacek Starz <jacek.starz@gmail.com>
2021-01-24 01:06:03 +0100

    🚀 Add auth service with backend communication

--- frontend/src/app/components/user-pages/login-page/login-page.component.ts
+++ frontend/src/app/components/user-pages/login-page/login-page.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from 'src/app/services/auth.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
+  returnUrl: string;
+
+  loginEmail: string;
+  loginPassword: string;
 
   registerPassword: string;
   registerConfirmPassword: string;
 
-
-  constructor(private router: Router, private _snackBar: MatSnackBar) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
+    this.authService.logout();
+
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
   validateData(): boolean {
@@ -27,17 +40,23 @@ export class LoginPageComponent implements OnInit {
 
   login(): void {
     // TODO: login user
-    this.router.navigate(['/profile']);
+    // this.router.navigate(['/profile']);
+    this.authService
+      .login(this.loginEmail, this.loginPassword)
+      .pipe(first())
+      .subscribe((data) => {
+        this.router.navigateByUrl(this.returnUrl);
+      });
   }
 
   register(): void {
     // TODO: register user
     if (this.validateData() == false) {
-      this._snackBar.open('Filled passwords are not the same!', 'Close',{
+      this._snackBar.open('Filled passwords are not the same!', 'Close', {
         duration: 3000,
       });
     } else {
-      this._snackBar.open('Successfully registered!', 'Close',{
+      this._snackBar.open('Successfully registered!', 'Close', {
         duration: 3000,
       });
       this.router.navigate(['/profile']);
---
+++ frontend/src/app/guards/auth.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private authService: AuthService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const userLoggedIn = this.authService.isLoggedIn();
+
+    if (!userLoggedIn) {
+      this.router.navigate(['/login'], {
+        queryParams: { returnUrl: state.url },
+      });
+    }
+
+    console.log(userLoggedIn);
+
+    return userLoggedIn;
+  }
+}
